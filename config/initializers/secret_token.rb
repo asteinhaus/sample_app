17d26b7b77ef6e72f3d65df35f9428de395dc6a8
asteinhaus <a.steinhaus@yahoo.de>
2011-06-08 12:25:28 +0200

    Just created rails project without TEST::UNIT (rails new sample_app -T)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '98cf62c49479510d051dd70a7fef4b5bb7fe3b1a17502ceb03055e96588ae9f7552758f214d3462ba8a748b9122b277f23e410dc21450d68484286d68c06a4e4'
